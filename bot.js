6400d27ab48b1bdf8ce8949c6f1717eada6af549
@ShotifyBot <aliflflin@gmail.com>
2026-07-17 17:33:59 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,13 @@ const { Bot } = require("grammy");
 
 const bot = new Bot("7866512180:AAG5p9E-GjufNb4c10oJuL2rq6lBUCoclNg");
 
-// ساده‌ترین دستور
 bot.command("start", async (ctx) => {
-    await ctx.reply("✅ ربات به درستی کار میکنه!");
-    console.log("✅ پیام start دریافت شد!");
+    await ctx.reply("✅ ربات کار میکنه!");
 });
 
-// هر پیامی رو جواب بده
 bot.on("message", async (ctx) => {
-    await ctx.reply("📩 پیام شما: " + ctx.message.text);
-    console.log("📩 پیام دریافت شد:", ctx.message.text);
+    await ctx.reply("📩 پیامت رو دریافت کردم: " + ctx.message.text);
 });
 
 bot.start();
-console.log("🚀 ربات تست اجرا شد!");console.error);
+console.log("ربات تست اجرا شد!");
